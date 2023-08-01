a9874ddf4b2d60806e054b66553a8364a8bbedc2
Harminder Singh Nijjar <110620707+harmindersinghnijjar@users.noreply.github.com>
2023-07-31 21:59:59 -0700

    Update llama2_model_downloader.py
    Add heavy commenting in ELI5 format

--- llama2_model_downloader.py
+++ llama2_model_downloader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# Import necessary libraries
 import os
 import requests
 import hashlib
@@ -7,22 +8,29 @@ from tqdm import tqdm
 
 # Function to download a file with a progress bar
 def download_file(url, filename):
+    # Send a request to the given URL to get the file
     response = requests.get(url, stream=True)
+    # Get the total size of the file from the 'content-length' header
     total = int(response.headers.get('content-length', 0))
+    # Open the file in binary write mode
     with open(filename, 'wb') as file, tqdm(
-            desc=filename,
-            total=total,
-            unit='iB',
-            unit_scale=True,
-            unit_divisor=1024,
+            desc=filename,             # Description for the progress bar
+            total=total,               # Total size of the file for the progress bar
+            unit='iB',                 # Unit for displaying file size
+            unit_scale=True,           # Scale the unit to KB, MB, etc.
+            unit_divisor=1024,         # Divisor for scaling the unit
         ) as bar:
         for data in response.iter_content(chunk_size=1024):
+            # Write the downloaded chunk to the file
             size = file.write(data)
+            # Update the progress bar with the size of the downloaded chunk
             bar.update(size)
 
+# Get the URL from the user
 print("Enter the URL: ")
 presigned_url = input()
 
+# Get the list of models to download from the user or use default values
 print("\nEnter the list of models to download without spaces (7B,13B,70B,7B-chat,13B-chat,70B-chat), or press Enter for all: ")
 model_size = input()
 target_folder = "."  # where all files should end up
@@ -31,15 +39,19 @@ os.makedirs(target_folder, exist_ok=True)
 if model_size == "":
     model_size = "7B,13B,70B,7B-chat,13B-chat,70B-chat"
 
+# Download the LICENSE and Acceptable Usage Policy files
 print("\nDownloading LICENSE and Acceptable Usage Policy")
 download_file(presigned_url.replace('*', "LICENSE"), os.path.join(target_folder, "LICENSE"))
 download_file(presigned_url.replace('*', "USE_POLICY.md"), os.path.join(target_folder, "USE_POLICY.md"))
 
+# Download the tokenizer files
 print("\nDownloading tokenizer")
 download_file(presigned_url.replace('*', "tokenizer.model"), os.path.join(target_folder, "tokenizer.model"))
 download_file(presigned_url.replace('*', "tokenizer_checklist.chk"), os.path.join(target_folder, "tokenizer_checklist.chk"))
 
+# Loop through the list of models and download them
 for model in model_size.split(","):
+    # Determine the shard and model path based on the selected model
     if model == "7B":
         shard = 0
         model_path = "llama-2-7b"
@@ -59,12 +71,15 @@ for model in model_size.split(","):
         shard = 7
         model_path = "llama-2-70b-chat"
         
+    # Print the model being downloaded
     print(f"\nDownloading {model_path}")
+    # Create the folder for the model if it doesn't exist
     os.makedirs(os.path.join(target_folder, model_path), exist_ok=True)
 
+    # Download each shard of the model
     for s in range(shard + 1):
         download_file(presigned_url.replace('*', f"{model_path}/consolidated.{str(s).zfill(2)}.pth"), os.path.join(target_folder, model_path, f"consolidated.{str(s).zfill(2)}.pth"))
 
+    # Download other files related to the model
     download_file(presigned_url.replace('*', f"{model_path}/params.json"), os.path.join(target_folder, model_path, "params.json"))
     download_file(presigned_url.replace('*', f"{model_path}/checklist.chk"), os.path.join(target_folder, model_path, "checklist.chk"))
-
